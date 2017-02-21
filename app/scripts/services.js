b1ee04c15913a35feaa51fa410d59a2baea0fd56
Eyuel Taddese <eyuel.taddese4@gmail.com>
2017-02-21 16:35:34 -0500

    Week 3 Angular ngRoute done

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -11,6 +11,7 @@ angular.module('confusionApp')
 
             var dishes=[
                 {
+                    _id:0,
                     name:'Uthapizza',
                     image: 'images/uthapizza.png',
                     category: 'mains',
@@ -52,6 +53,7 @@ angular.module('confusionApp')
                     ]
                 },
                 {
+                    _id:1,
                     name:'Zucchipakoda',
                     image: 'images/zucchipakoda.png',
                     category: 'appetizer',
@@ -93,6 +95,7 @@ angular.module('confusionApp')
                     ]
                 },
                 {
+                    _id:2,
                     name:'Vadonut',
                     image: 'images/vadonut.png',
                     category: 'appetizer',
@@ -134,6 +137,7 @@ angular.module('confusionApp')
                     ]
                 },
                 {
+                    _id:3,
                     name:'ElaiCheese Cake',
                     image: 'images/elaicheesecake.png',
                     category: 'dessert',
