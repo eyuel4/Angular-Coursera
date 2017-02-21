aee4ef928a9c70b432cc21690c5728af8e698ba2
Eyuel Taddese <eyuel.taddese4@gmail.com>
2017-02-21 16:45:18 -0500

    Week 3 Angular Ui appconfig

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,26 @@
 'use strict';
 
-angular.module('confusionApp', ['ngRoute'])
-    .config(function ($routeProvider) {
-        $routeProvider
+angular.module('confusionApp', ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-            // route for the contactus page
-            .when('/contactus', {
-                templateUrl : 'contactus.html',
-                controller : 'ContactController'
+            // route for the home page
+            .state('app', {
+                url: '/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/header.html',
+                    },
+                    'content': {
+                        templateUrl : '<h1>To be Completed </h1>',
+                        controller : 'IndexController'
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.html'
+                    }
+                }
             })
-
-            // route for the menu page
-            .when('/menu', {
-                templateUrl : 'menu.html',
-                controller : 'MenuController'
-            })
-
-            // route for the DishDetail page
-            .when('/menu/:id', {
-                templateUrl : 'dishdetail.html',
-                controller : 'DishDetailController'
-            })
-            .otherwise('/contactus');
+        
     })
+
 ;
